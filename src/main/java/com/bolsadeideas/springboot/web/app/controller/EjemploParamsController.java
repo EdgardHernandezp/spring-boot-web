e82af12bab096d69e61a1e7d7b8e67ecc65b5b00
Dreamseeker5 <edgardhernandezp@gmail.com>
2019-07-10 21:32:04 -0400

    Passing multiple arguments to a method handler. Using RequestParam and HTTPServletRequest.

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,25 @@ public class EjemploParamsController {
 	@GetMapping("/string")
 	public String param(@RequestParam(required=false, defaultValue="ALOHAA") String texto, Model model) {
 		model.addAttribute("resultado", "Parametro enviado: " + texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "Saludo: " + saludo + " , numero: " + numero);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (NumberFormatException e) {
+
+		}
+		model.addAttribute("resultado", "Saludo: " + saludo + " , numero: " + numero);
 		return "params/ver";
 	}
 }
